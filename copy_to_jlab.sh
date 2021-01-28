cb5d9caf9292048afa6d02ab91bae9bfe9c044ac
robertej19 <robertej@mit.edu>
2021-01-27 20:07:45 -0500

    cleaned up code for general use

--- copy_to_jlab.sh
+++ copy_to_jlab.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 password="$(cat ~/scripts/.pass/.jlab)"
+username="$(cat ~/scripts/.pass/.username)"
 
 echo "Usage: Enter full path of file or directory to be copied from local to remote"
 echo "Example: ./copy_to_jlab.sh /home/bobby/scripts/test.txt"
 
 loc=$1
 
-sshpass -p "$password" scp -r $loc robertej@ftp.jlab.org:/u/home/robertej/
+sshpass -p "$password" scp -r $loc $username@ftp.jlab.org:/u/home/$username/
 
 echo '...'
 
